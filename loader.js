1122a1d18b666db1d71d8bbb620694add88c38c6
liubinpeng <1611274445@qq.com>
2017-07-18 16:48:14 +0800

    add

--- loader.js
+++ loader.js
@@ -1,11 +1,12 @@
 function loadingWithMessage($message) {
-
+	console.log("show")
 	$("body").on("touchmove", function(event) {
 		event.preventDefault;
 	}, false);
 
-	$('<div class="loader">' +
-		'<div class="loader-inner ball-spin-fade-loader" style="position: absolute;left: 50%;top: 50%;">' +
+	if($message == "" || $message == undefined || $message == null){
+		$('<div class="loader">' +
+		'<div class="loader-inner ball-spin-fade-loader" style="position: absolute;left: calc((100% - 20px)/2);top: 50%;">' +
 		'<div></div>' +
 		'<div></div>' +
 		'<div></div>' +
@@ -15,8 +16,23 @@ function loadingWithMessage($message) {
 		'<div></div>' +
 		'<div></div>' +
 		'</div>' +
-		'<p style="position: absolute;left: 0%;top: 55%;width: 100%;text-align: center;font-size:1.7em;">' + $message + '</p>' +
 		'</div>').appendTo($("body"));
+	}else{
+		$('<div class="loader">' +
+		'<div class="loader-inner ball-spin-fade-loader" style="position: absolute;left: calc((100% - 20px)/2);top: 45%;">' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'<div></div>' +
+		'</div>' +
+		'<p style="position: absolute;left: 0%;top: 55%;width: 100%;text-align: center;font-size:17px;color: #d71e5d !important;">' + $message + '</p>' +
+		'</div>').appendTo($("body"));
+	}
+	
 }
 
 function dismiss() {
--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-(function() {
+(function(){
 	loadingWithMessage("正在请求数据");
-	setTimeout(function(){
-		console.log("模拟请求接口或者耗时操作");
-		dismiss();
-	},3000);
-
+	
 
 })();
