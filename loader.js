e63f0e0e22ec435170297a342b6269104dcb18b5
liubinpeng <1611274445@qq.com>
2017-07-27 21:19:06 +0800

    animations

--- loader.js
+++ loader.js
@@ -1,41 +1,62 @@
-function loadingWithMessage($message) {
-	console.log("show")
-	$("body").on("touchmove", function(event) {
-		event.preventDefault;
-	}, false);
+function loadingWithMessage($message, $type) {
+    $("body").on("touchmove", function(event) {
+        event.preventDefault;
+    }, false);
+    var animationCss = "";
+    if ($type == 1){
+        animationCss = "ball-spin-fade-loader";
+    }else if($type == 2){
+        animationCss = "line-spin-fade-loader";
+    }else if($type == 3){
+		animationCss = "semi-circle-spin";
+    }else if($type == 4){
+		animationCss = "ball-clip-rotate-pulse";
+    }else if($type == 5){
+		animationCss = "ball-clip-rotate-multiple";
+    }else if($type == 6){
+		animationCss = "line-scale";
+    }else if($type == 7){
+		animationCss = "pacman";
+    }else if($type == 8){
+		animationCss = "line-scale-party";
+    }else if($type == 9){
+		animationCss = "ball-zig-zag-deflect";
+    }
 
-	if($message == "" || $message == undefined || $message == null){
-		$('<div class="loader">' +
-		'<div class="loader-inner ball-spin-fade-loader" style="position: absolute;left: calc((100% - 20px)/2);top: 50%;">' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'</div>' +
-		'</div>').appendTo($("body"));
-	}else{
-		$('<div class="loader">' +
-		'<div class="loader-inner ball-spin-fade-loader" style="position: absolute;left: calc((100% - 20px)/2);top: 45%;">' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'<div></div>' +
-		'</div>' +
-		'<p style="position: absolute;left: 0%;top: 55%;width: 100%;text-align: center;font-size:17px;color: #d71e5d !important;">' + $message + '</p>' +
-		'</div>').appendTo($("body"));
-	}
-	
+
+    if ($message == "" || $message == undefined || $message == null) {
+        $('<div class="loader">' +
+            '<div class="loader-inner ' + animationCss + '" style="position: absolute;left: calc((100% - 20px)/2);top: 50%;">' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '</div>' +
+            '</div>').appendTo($("body"));
+    } else {
+        $('<div class="loader">' +
+            '<div class="loader-inner ' + animationCss + '" style="position: absolute;left: calc((100% - 20px)/2);top: 45%;">' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '<div></div>' +
+            '</div>' +
+            '<p style="position: absolute;left: 0%;top: 55%;width: 100%;text-align: center;font-size:17px;color: #d71e5d !important;">' + $message + '</p>' +
+            '</div>').appendTo($("body"));
+    }
+
+
 }
 
 function dismiss() {
-	$(".loader").remove();
-	$("body").off("touchmove");
+    $(".loader").remove();
+    $("body").off("touchmove");
 }
--- test.js
+++ test.js
@@ -1,10 +1,72 @@
-(function(){
-	loadingWithMessage("正在请求数据");
+(function() {
+
+    $("#btn1").click(function() {
+        loadingWithMessage("正在请求数据",1);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn2").click(function() {
+        loadingWithMessage("正在请求数据",2);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn3").click(function() {
+        loadingWithMessage("正在请求数据",3);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    
+    $("#btn4").click(function() {
+        loadingWithMessage("正在请求数据",4);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn5").click(function() {
+        loadingWithMessage("正在请求数据",5);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn6").click(function() {
+        loadingWithMessage("正在请求数据",6);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn7").click(function() {
+        loadingWithMessage("正在请求数据",7);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+    $("#btn8").click(function() {
+        loadingWithMessage("正在请求数据",8);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+     $("#btn9").click(function() {
+        loadingWithMessage("正在请求数据",9);
+        setTimeout(function() {
+           dismiss();
+        }, 2000);
+    });
+
+   
 
 
-	setTimeout(function(){
-		dismiss();
-	},4000);
-	
+
 
 })();
